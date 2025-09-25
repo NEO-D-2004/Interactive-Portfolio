d43d879fa17cffa91144696562f2df5d74eb78fa
NEO DHANUSH <k.a.dhanush008@gmail.com>
2025-09-25 13:36:20 +0530

    Initial Commiti

---
+++ main.js
@@ -0,0 +1,32 @@
+VANTA.GLOBE({
+  el: "#hero",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xffffff,
+  backgroundColor: 0x0
+})
+
+// Highlight active nav link
+const navLinks = document.querySelectorAll(".navbar ul li a");
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
+
+
+// Auto typing effect
+const typed = new Typed('.auto-type', {
+            strings: ['UI/UX Designing', 'FrontEnd Developer','Web Development','Graphic Designing'],
+            typeSpeed: 90,
+            backSpeed: 90,
+            loop: true
+            
+});
